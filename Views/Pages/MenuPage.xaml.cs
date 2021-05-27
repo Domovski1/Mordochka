4d72f64992d9ac00a9a137fc897418136228517e
Domovski1 <624273242@mail.ru>
2021-05-27 23:46:06 +0300

    Изменения инициализаций старниц (совет Дани)

--- Views/Pages/MenuPage.xaml.cs
+++ Views/Pages/MenuPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Mordochka.Views.Pages
 {
@@ -25,9 +15,11 @@ namespace Mordochka.Views.Pages
             InitializeComponent();
         }
 
+
+        static ClientsPage clients = new ClientsPage();
         private void ClientsList_Btn(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new ClientsPage());
+            NavigationService.Navigate(clients);
         }
     }
 }
