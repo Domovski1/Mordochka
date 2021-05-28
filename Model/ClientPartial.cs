b87310bb887b514d336447dcfc7644201818bee1
Domovski1 <624273242@mail.ru>
2021-05-28 17:26:35 +0300

    Всякие изменения

---
+++ Model/ClientPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mordochka.Model
+{
+    public partial class ClientService
+    {
+        public string CountVisit
+        {
+            get
+            {
+                return AppData.db.ClientService.Where(x => x.ClientID == this.ID).Count().ToString();
+                ;
+            } 
+        }
+    }
+}
