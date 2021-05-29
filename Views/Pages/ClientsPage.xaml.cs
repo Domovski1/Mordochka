32a158dc92d549b5f0ff057a721b427a3560f11d
Domovski1 <624273242@mail.ru>
2021-05-29 10:36:06 +0300

    Немного оптимизрован код с выводом данных

--- Views/Pages/ClientsPage.xaml.cs
+++ Views/Pages/ClientsPage.xaml.cs
@@ -25,18 +25,29 @@ namespace Mordochka.Views.Pages
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //Clients_Grid.ItemsSource = AppData.db.Client.ToList();
-            Clients_Grid.ItemsSource = AppData.db.ClientService.ToList();
-            var Col = AppData.db.Client.Count().ToString();
-            Col += "/" + (Clients_Grid.Items.Count - 1).ToString();
-            Count_Tblock.Text = Col;
+            Clients_Grid.ItemsSource = AppData.db.Client.ToList();
+            //Clients_Grid.ItemsSource = AppData.db.ClientService.ToList();
+            Count_Tblock.Text = (Clients_Grid.Items.Count - 1).ToString() + "/" + AppData.db.Client.Count().ToString();
             ;
         }
 
         private void Search_Txb_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            //Clients_Grid.ItemsSource = AppData.db.ClientService.Where(item => item.FirstName.Contains(Search_Txb.Text)).ToList();
+        { 
             Count_Tblock.Text = (Clients_Grid.Items.Count - 1).ToString() + "/" + AppData.db.Client.Count().ToString();
+            var Data = AppData.db.Client.Where(x => x.FirstName == Search_Txb.Text || x.FirstName.Contains(Search_Txb.Text)).ToList();
+            if (Data != null)
+            {
+                Clients_Grid.ItemsSource = Data;
+            } else
+            {
+
+            }
+        }
+
+        private void UpdateClient_Click(object sender, RoutedEventArgs e)
+        {
+            //var selectedClient = Clients_Grid.SelectedItem as Client;
+            NavigationService.Navigate(new ClientCreateUpdatePage((Client)Clients_Grid.SelectedItem));
         }
     }
 }
