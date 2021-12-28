ec9461b681aa301e65c9d4077383e30d896a4767
Domovski1 <624273242@mail.ru>
2021-12-28 23:31:59 +0300

    Model -> Исправлена ошибка для вывода кол-ва посещений клиента

--- Model/ClientPartial.cs
+++ Model/ClientPartial.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Mordochka.Model
 {
-    public partial class ClientService
+    public partial class Client
     {
         public string CountVisit
         {
